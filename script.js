ce8f13aa508bcb69cd09277147d99fa7e0da40b3
Jonathas Souza <jonathassouza14@gmail.com>
2023-08-19 01:40:16 -0300

    aa

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function enviar(){
     let res = document.getElementById("res")
     res.innerText = "Tudo Funcionando perfeitamente!"
+    res.style.color = ('white')
     alert('Ainda esta em desenvolvimento!')
 }
